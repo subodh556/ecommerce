03379adbe91e2f222edd878029b96ceec5de3f0b
Subodh <sohonisubodh@gmail.com>
2024-01-31 11:09:55 +0530

    navigation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import "./index.css";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )

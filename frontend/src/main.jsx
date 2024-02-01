f1500cce276f444cf5c4215ed5ccf20577624140
Subodh <sohonisubodh@gmail.com>
2024-02-01 23:00:49 +0530

    category

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import Register from "./pages/Auth/Register";
 import Profile from './pages/User/Profile.jsx';
 import AdminRoute from "./pages/Admin/AdminRoute.jsx"
 import UserList from './pages/Admin/UserList.jsx';
+import CategoryList from './pages/Admin/CategoryList.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
 
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />}/>
+        <Route path="categorylist" element={<CategoryList />}/>
       </Route>
 
     </Route>

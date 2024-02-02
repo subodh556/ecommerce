d2c2ec62f7242fe29a1091d7924cef2d7be1ceb2
Subodh <sohonisubodh@gmail.com>
2024-02-02 15:35:51 +0530

    products

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,8 @@ import AdminRoute from "./pages/Admin/AdminRoute.jsx"
 import UserList from './pages/Admin/UserList.jsx';
 import CategoryList from './pages/Admin/CategoryList.jsx';
 import ProductList from './pages/Admin/ProductList.jsx';
+import AllProducts from './pages/Admin/AllProducts.jsx';
+import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,7 +31,11 @@ const router = createBrowserRouter(
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />}/>
         <Route path="categorylist" element={<CategoryList />}/>
-        <Route path="productlist" element={<ProductList />}/>
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />}/>
+        <Route path="productlist/:pageNumber" element={<ProductList />}/>
+        <Route path="product/update/:_id" element={<ProductUpdate />}/>
+        
       </Route>
 
     </Route>

92df1b7ae7da988f75941bb2faa291f227b884ac
Subodh <sohonisubodh@gmail.com>
2024-02-03 23:13:48 +0530

    cart

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -13,7 +13,8 @@ const Navigation = () => {
     const { userInfo } = useSelector((state) => state.auth);
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showSidebar, setShowSidebar] = useState(false);
-  
+    const { cartItems } = useSelector((state) => state.cart);
+    
     const toggleDropdown = () => {
       setDropdownOpen(!dropdownOpen);
     };
@@ -67,7 +68,16 @@ const Navigation = () => {
                 <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
                 <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
               </div>
-    
+              
+          <div className="absolute top-9">
+            {cartItems.length > 0 && (
+              <span>
+                <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                  {cartItems.reduce((a, c) => a + c.qty, 0)}
+                </span>
+              </span>
+            )}
+          </div>
             </Link>
     
             <Link to="/favorite" className="flex relative">

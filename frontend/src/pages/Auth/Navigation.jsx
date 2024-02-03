194c29058254846a2615be0167399a5c9964f085
Subodh <sohonisubodh@gmail.com>
2024-02-03 11:38:33 +0530

    commit

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -7,7 +7,7 @@ import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../../redux/api/usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
-
+import FavoritesCount from "../Products/FavoritesCount";
 
 const Navigation = () => {
     const { userInfo } = useSelector((state) => state.auth);
@@ -76,7 +76,7 @@ const Navigation = () => {
                 <span className="hidden nav-item-name mt-[3rem]">
                   Favorites
                 </span>{" "}
-                
+                <FavoritesCount/>
               </div>
             </Link>
           </div>

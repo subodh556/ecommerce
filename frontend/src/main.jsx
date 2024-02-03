3173950ffd872d8be41f8c60cf2c67466d7430f1
Subodh <sohonisubodh@gmail.com>
2024-02-03 12:55:29 +0530

    product details

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,6 +20,7 @@ import AllProducts from './pages/Admin/AllProducts.jsx';
 import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 import Home from "./Home.jsx"
 import Favorites from './pages/Products/Favorites.jsx';
+import ProductDetails from './pages/Products/ProductDetails.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
       <Route path="/register" element={<Register />} />
       <Route index={true} path="/" element={<Home />} />
       <Route path="/favorite" element={<Favorites />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />

919f50e95fc36359d99f9a0144762c94f5b87fb1
Subodh <sohonisubodh@gmail.com>
2024-02-06 22:30:08 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,14 +11,4 @@
 // };
 
 // export default connectDB;
-import mongoose from "mongoose";
-const connectDB=async ()=>{
-  try{
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Successfully connected to database 👍 ")
-  }
-  catch(error){
-      console.log(`Error:${error.message}`);
-  }
-  
-}
+

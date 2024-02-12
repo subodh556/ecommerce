8fd70bd5f7a37aeb687a32c24e931c02b9b8405c
Subodh <sohonisubodh@gmail.com>
2024-02-12 14:25:16 +0530

    commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "http://127.0.0.1:8000/";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";

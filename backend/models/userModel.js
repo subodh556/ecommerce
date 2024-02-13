94a1b7b04769b10b00994ff5f715ff6e04edacbf
Subodh <sohonisubodh@gmail.com>
2024-02-13 23:06:42 +0530

    commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
-   
+    username: {
+      type: String,
+      required: true,
+    },
 
     email: {
       type: String,

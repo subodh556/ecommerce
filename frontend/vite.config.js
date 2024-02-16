fdcdf6c9f363470b50c3d38858df8051d66f8333
Subodh <sohonisubodh@gmail.com>
2024-02-16 20:52:53 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://ecommerce-3sp1thdhv-nextjsauths-projects.vercel.app/",
-      "/uploads/": "https://ecommerce-3sp1thdhv-nextjsauths-projects.vercel.app/",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 })

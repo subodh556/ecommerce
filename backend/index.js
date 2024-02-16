d4d5104d83b8fb69226eac7ee9806827000b5e2c
Subodh <sohonisubodh@gmail.com>
2024-02-16 19:23:58 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,13 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+const cors = require('cors');
 
+app.use(cors({
+  origin:[""],
+  methods:["POST","GET","PUT","DELETE"],
+  credentials:true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

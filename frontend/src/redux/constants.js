f017eb53bb533c00ec8d43f0751da3af37980c89
Subodh <sohonisubodh@gmail.com>
2024-02-16 21:17:11 +0530

    commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://ecommerce-chrj.vercel.app/";
+export const BASE_URL = "https://ecommerce-chrj.vercel.app/api";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";

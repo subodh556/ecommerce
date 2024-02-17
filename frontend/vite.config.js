523bb63137d0a4f47c47fa385dc6c30894a896dc
Subodh <sohonisubodh@gmail.com>
2024-02-17 10:49:42 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend/",
   plugins: [react()], 
   server: {
+    https: true,
+    port: 5000,
     proxy: {
       "/api/": "https://ecommerce-theta-hazel.vercel.app/",
       "/uploads/": "https://ecommerce-theta-hazel.vercel.app/",
     },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets', 
+    }
   },
 })
+

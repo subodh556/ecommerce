2fabf2df03f01dd96091311952aeb807186b09ce
Subodh <sohonisubodh@gmail.com>
2024-02-18 10:54:36 +0530

    commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,7 +90,7 @@ const getAllOrders = async (req, res) => {
 
 const getUserOrders = async (req, res) => {
   try {
-    const orders = await Order.find({ user: req.user._id });
+    const orders = await Order.find({ user: req.user._id }).sort({paidAt:-1});
     res.json(orders);
   } catch (error) {
     res.status(500).json({ error: error.message });

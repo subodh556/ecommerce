53db4d8e376b1d07d9c34b9bee03d19e6a08b71d
Subodh <sohonisubodh@gmail.com>
2024-02-19 11:33:20 +0530

    favorites

--- frontend/src/pages/Products/HeartIcon.jsx
+++ frontend/src/pages/Products/HeartIcon.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { FaHeart, FaRegHeart} from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import {
   addToFavorites,
   removeFromFavorites,
@@ -14,25 +15,31 @@ import {
 } from "../../Utils/localStorage";
 
 const HeartIcon = ({ product }) => {
+  const navigate=useNavigate();
   const dispatch = useDispatch();
   const favorites = useSelector((state) => state.favorites) || [];
   const isFavorite = favorites.some((p) => p._id === product._id);
-
+  const { userInfo } = useSelector((state) => state.auth);
   useEffect(() => {
     const favoritesFromLocalStorage = getFavoritesFromLocalStorage();
     dispatch(setFavorites(favoritesFromLocalStorage));
   },[] );
 
   const toggleFavorites = () => {
-    if (isFavorite) {
-      dispatch(removeFromFavorites(product));
-      // remove the product from the localStorage as well
-      removeFavoriteFromLocalStorage(product._id);
-    } else {
-      dispatch(addToFavorites(product));
-      // add the product to localStorage as well
-      addFavoriteToLocalStorage(product);
+    if(userInfo){
+      if (isFavorite) {
+        dispatch(removeFromFavorites(product));
+        // remove the product from the localStorage as well
+        removeFavoriteFromLocalStorage(product._id);
+      } else {
+        dispatch(addToFavorites(product));
+        // add the product to localStorage as well
+        addFavoriteToLocalStorage(product);
+      }
+    }else{
+      navigate("/login")
     }
+    
   };
 
   return (
@@ -40,7 +47,7 @@ const HeartIcon = ({ product }) => {
       className="absolute top-2 right-5 cursor-pointer"
       onClick={toggleFavorites}
     >
-      {isFavorite ? (
+      {(isFavorite && userInfo) ? (
         <FaHeart className="text-pink-500" />
       ) : (
         <FaRegHeart className="text-white" />

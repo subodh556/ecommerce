8b3ef773bb07c06e8a42d0016c97a003b00a0a66
Subodh <sohonisubodh@gmail.com>
2024-02-22 23:17:55 +0530

    commit

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,10 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  userInfo: localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
-    : null,
-};
 
 const authSlice = createSlice({
   name: "auth",

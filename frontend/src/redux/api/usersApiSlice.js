08a0630da4cd774b1eba0654f92f3ffc66fe5e1b
Subodh <sohonisubodh@gmail.com>
2024-02-25 22:34:35 +0530

    commit

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -3,7 +3,13 @@ import { USERS_URL } from "../constants";
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-   
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
     register: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}`,
